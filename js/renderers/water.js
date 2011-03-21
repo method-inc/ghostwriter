622bfc7e3c8dd2712e779f37ca25bd4eb7ff8b84
Hunter Loftis <hunter.loftis+github@gmail.com>
2011-03-21 14:47:49 -0400

    Water renderer now has settings

--- js/renderers/water.js
+++ js/renderers/water.js
@@ -2,114 +2,131 @@
 
   var ns = 'waterRenderer';
   
-  var lastTip = null;
-  
-  function Drop(ctx, position, radius, direction) {
-    var self = this;
-    
-    this.ctx = ctx;
-    this.pos = position;
-    this.dir = {x: direction.x * .05, y: direction.y * .05};
-    this.r = radius;
-    this.opacity = 0.02;
-    
-    this.debug = $('#debugboard')[0].getContext('2d');
-    
-    window.setTimeout(function() {self.update();}, 1000 / 25);
-  }
-  Drop.prototype = {
-    update: function() {
-      var self = this;
-      this.debug.clearRect(this.pos.x - 3, this.pos.y - 3, 6, 6);
-      this.pos.y += 3;
-      this.pos.x += this.dir.x;
-      this.pos.y += this.dir.y;
-      this.r += 5;
-      this.opacity -= 0.001;
-      if (this.opacity > 0.001) {
-        
-        this.ctx.save();
-        this.ctx.translate(this.pos.x, this.pos.y);
-        this.ctx.fillStyle = 'rgba(0, 20, 30, ' + this.opacity + ')';
-        this.ctx.beginPath();
-        this.ctx.arc(0, 0, this.r, 0, 2 * Math.PI, false);
-        //this.ctx.fillRect(this.r * -.5, this.r * -.5, this.r, this.r);
-        this.ctx.closePath();
-        this.ctx.fill();
-        this.ctx.restore();
-        
-        this.debug.fillStyle = '#0ff';
-        this.debug.fillRect(this.pos.x - 2, this.pos.y - 2, 4, 4);
-        window.setTimeout(function() {self.update();}, 1000 / 25);
-      }
-    }
-  }
-  
   function WaterRenderer(canvas, options) {
-    options = options || {};
-    
     var self = this;
+    
+    this.settings = {
+      spacing: 15,
+      scatter: 10,
+      fps: 25,
+      momentum: 0.05,
+      radius: 10,
+      spread: 5,
+      gravity: 3,
+      opacity: 0.02,
+      fade: 0.001,
+      color: [0,0,0]
+    };
+    
+    $.extend(this.settings, options || {})
     
     this.ctx = canvas.ctx;
-    this.origPos = null;
-    this.targetPos = null;
+    this.position = [null, null];                                 // [from, to]
     this.dripping = false;
-    
-    this.spacing = options.spacing || 15;
-    this.scatter = options.scatter || 10;
-    this.fps = options.fps || 25;
-    this.momentum = options.momentum || 0.05;
-    this.dropsize = options.dropsize || 10;
-    this.spread = options.spread || 5;
-    this.gravity = options.gravity || 2;
-    
     
     this.debug = $('#debugboard')[0].getContext('2d');
     
     $(document).bind(ns + '.down', function(event, position) {
-      self.targetPos = {x: position.x, y: position.y};
-      self.origPos = {x: position.x, y: position.y};
+      self.position = [{x: position.x, y: position.y}, {x: position.x, y: position.y}];
       self.startDrip();
     });
     
     $(document).bind(ns + '.move', function(event, position) {
-      self.origPos = self.targetPos;
-      self.targetPos = {x: position.x, y: position.y};
+      self.position.shift();
+      self.position.push({x: position.x, y: position.y});
     });
     
     $(document).bind(ns + '.up', function(event, position) {
       self.stopDrip();
-    });
-    
+    });  
   }
+  
   WaterRenderer.prototype = {
+    
     startDrip: function() {
-      if (this.dripping) {
-        return;
-      }
+      if (this.dripping) return;
+      this.dripping = true;  
       var self = this;
-      this.dripping = true;
+      
+      // Update loop
+      
       (function update() {
         if (self.dripping) {
-          var dx = self.targetPos.x - self.origPos.x,
-              dy = self.targetPos.y - self.origPos.y,
+          var dx = self.position[1].x - self.position[0].x,
+              dy = self.position[1].y - self.position[0].y,
               dist = Math.sqrt(dx * dx + dy * dy) || 1,
-              spacing = 15,
+              spacing = self.settings.spacing,
               drops = dist / spacing,
               ratio = spacing / dist;
+              
           for (var i = 0; i <= drops; i++) {
-            var pos = {x: self.targetPos.x - ratio * dx * i + Math.random() * 20 - 10, y: self.targetPos.y - ratio * dy * i + Math.random() * 20 - 10};
-            new Drop(self.ctx, pos, 10, {x: dx, y: dy});
+            var pos = {
+                  x: self.position[1].x - ratio * dx * i,
+                  y: self.position[1].y - ratio * dy * i
+                },
+                vel = {x: dx, y: dy};
+            new Drop(self.ctx, pos, vel, self.settings);
           }
-          window.setTimeout(update, 1000 / 25);
+          
+          window.setTimeout(update, 1000 / self.settings.fps);
         }
       })();
     },
+    
     stopDrip: function() {
       this.dripping = false;
     }
   }
   
   window.WaterRenderer = WaterRenderer;
+  
+  // Helper constructors
+  
+  function Drop(ctx, position, velocity, settings) {
+    var self = this;
+    
+    this.ctx = ctx;
+    this.pos = position;
+    this.vel = {x: velocity.x * settings.momentum, y: velocity.y * settings.momentum};
+    this.r = settings.radius;
+    this.settings = settings;
+    this.opacity = settings.opacity;
+    this.colorString = 'rgba(' + settings.color.join(',') + ',';
+    
+    this.debug = $('#debugboard')[0].getContext('2d');
+    
+    window.setTimeout(function() {self.update();}, 1000 / settings.fps);
+  }
+  
+  Drop.prototype = {
+    update: function() {
+      var self = this;
+      
+      this.debug.clearRect(this.pos.x - 3, this.pos.y - 3, 6, 6);
+      
+      this.pos.y    += this.settings.gravity;
+      this.pos.x    += this.vel.x;
+      this.pos.y    += this.vel.y;
+      this.r        += this.settings.spread;
+      this.opacity  -= this.settings.fade;
+      
+      if (this.opacity > this.settings.fade) {
+        
+        this.ctx.save();
+          this.ctx.translate(this.pos.x, this.pos.y);
+          this.ctx.fillStyle = this.colorString + this.opacity + ')';
+          this.ctx.beginPath();
+          this.ctx.arc(0, 0, this.r, 0, 1.8 * Math.PI, false);
+          this.ctx.closePath();
+          this.ctx.fill();
+        this.ctx.restore();
+        
+        this.debug.fillStyle = '#0ff';
+        this.debug.fillRect(this.pos.x - 2, this.pos.y - 2, 4, 4);
+        
+        window.setTimeout(function() {self.update();}, 1000 / this.settings.fps);
+      }
+    }
+  }
 
 })(jQuery, ko);
